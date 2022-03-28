c5e8ee50d515c3e1d171e128b5d455d22057cf33
Rajni Rajoriya <rajniraoriya72@gmail.com>
2022-03-28 18:13:06 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from sketchpy import library as lib
+
+# obj = lib.apj() #APJ Abdul Kalam
+# obj = lib.rdj()  # rdj means Robert Downey JR.
+# obj = lib.bts()  # Bangtan Boys, is a South Korean boy bond
+# obj = lib.flag()
+# obj = lib.gojo()  # Satoru Gojo is a fictional character from Gege Akutami's manga Jujutsu Kaisen
+# obj = lib.ironman_ascii()
+# obj = lib.tom_holland() # English movies actor
+obj = lib.vijay()  # South movies actor
+obj.draw()
